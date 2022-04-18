3188392853eab685c4fe50d013c8b0846d868893
Carina Druce <trueblue546@gmail.com>
2022-04-18 20:42:49 +0100

    first commit, 1 test still not working

---
+++ tests/github.test.js
@@ -0,0 +1,20 @@
+const GitAccount = require("../classes/github.js");
+const axios = require("axios");
+const OCTOCAT_DATA = require("../mocks/github_stats.js");
+
+jest.mock("axios");
+
+describe("GitHub API getUserData method", () => {
+  it("should return back octocat's data", async () => {
+    axios.get.mockResolvedValue(OCTOCAT_DATA);
+    const response = await new GitAccount().getUserData("octocat");
+    console.log(repsonse);
+    expect(data.repos).toEqual(8);
+  });
+
+  it("should throw an error if API call fails", async () => {
+    axios.get.mockRejectedValue(new Error());
+    const account = new GitAccount();
+    await expect(account.getUserData("octocat")).rejects.toThrow("new error");
+  });
+});
